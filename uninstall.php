cb225aae68dbf1fafda6b1adb722ca0635c0e170
te_taipo <te_taipo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-05 12:42:46 +0000

    Updated uninstall.php
    git-svn-id: https://plugins.svn.wordpress.org/pareto-security/trunk@1192762 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -1,14 +1,4 @@
 <?php
- /*
-  The user function for uninstalling Pareto Security WordPress plugin
-  Plugin Name: Pareto Security
-  Plugin URI: http://hokioisec7agisc4.onion/?p=25
-  Description: Core Security Class - Defense against a range of common attacks such as database injection
-  Author: Te_Taipo
-  Version: 1.0.3
-  Author URI: http://hokioisec7agisc4.onion
-  BTC:1LHiMXedmtyq4wcYLedk9i9gkk8A8Hk7qX
-  */
 
 if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
     header( 'Status: 403 Forbidden' );
